5586a8a8c261ff9bbba5810d6e53678a2f9abcc5
Filip <s097070@student.tu.kielce.pl>
2025-12-04 19:21:03 +0100

    dzialaja popupy itd

--- src/main/java/com/example/projekt/SceneController.java
+++ src/main/java/com/example/projekt/SceneController.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import models.ConnectDB;
 
@@ -19,8 +20,15 @@ public class SceneController {
 
     @FXML
     private TextField login;
+
+    @FXML
+    private TextField login2;
+
     @FXML
     private PasswordField password;
+
+    @FXML
+    private PasswordField password2;
 
     @FXML
     public void SwichToMenu(javafx.event.ActionEvent event) throws IOException {
@@ -48,12 +56,41 @@ public class SceneController {
 
     @FXML
     public void SwichToMainMenu(javafx.event.ActionEvent event) throws IOException {
+        if (login2.getText().isEmpty() || password2.getText().isEmpty()) {
+
+            Parent root2 = FXMLLoader.load(
+                    getClass().getResource("/com/example/projekt/BladLogowania.fxml")
+            );
+
+            Stage popup = new Stage();
+            popup.setTitle("Błąd logowania");
+
+            popup.initModality(Modality.APPLICATION_MODAL);
+
+            popup.setScene(new Scene(root2));
+            popup.show();
+
+            return;
+        }
         Parent root = FXMLLoader.load(
                 getClass().getResource("/com/example/projekt/EkranPoZalogowaniu.fxml")
         );
 
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @FXML
+    public void SwichToMainMenu2(javafx.event.ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(
+                getClass().getResource("/com/example/projekt/EkranPoZalogowaniu.fxml")
+        );
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
@@ -68,22 +105,55 @@ public class SceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
     }
 
     @FXML
     public void SwichToWypozycz(javafx.event.ActionEvent event) throws IOException {
+
         Parent root = FXMLLoader.load(
                 getClass().getResource("/com/example/projekt/EkranWypozyczania.fxml")
         );
-
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
     }
 
     @FXML
-    public void SaveNewUser(javafx.event.ActionEvent event){
-        db.insertNewUser(login.getText(),password.getText());
+    public void SaveNewUser(javafx.event.ActionEvent event)throws IOException{
+        if (login.getText().isEmpty() || password.getText().isEmpty()) {
+            Parent root2 = FXMLLoader.load(
+                    getClass().getResource("/com/example/projekt/BladLogowania.fxml")
+            );
+
+            Stage popup = new Stage();
+            popup.setTitle("Błąd logowania");
+
+            popup.initModality(Modality.APPLICATION_MODAL);
+
+            popup.setScene(new Scene(root2));
+            popup.show();
+
+            return;
+        }
+        else {
+            db.insertNewUser(login.getText(), password.getText());
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/com/example/projekt/EkranLogowania.fxml")
+            );
+
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    @FXML
+    public void PopupReturn(javafx.event.ActionEvent event){
+        Stage popup = (Stage)((Node) event.getSource()).getScene().getWindow();
+        popup.close();
     }
 }
